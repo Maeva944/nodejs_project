2da08af666fafc3d64bf78200c9ecdafe17aba76
Maeva Nono <maeva.nono@efrei.net>
2025-03-28 08:42:35 +0100

    docker fonctionne

--- backend/model/Auth.js
+++ backend/model/Auth.js
@@ -44,7 +44,7 @@ Router.post("/", async(req , res) => {
         const token = jwt.sign(
             {id: user.id, role: user.role},
             process.env.JWT_SECRET,
-            {expiresIn: 10}
+            {expiresIn: 3600}
         );
 
         res.json({token});
